661123cefd332c213a5c15894317d896d9bcf8b1
Entynetproject <43011806+entynetproject@users.noreply.github.com>
2019-06-26 12:09:57 +0300

    Delete banner9.py

--- banner/banner9.py
+++
@@ -1,8 +0,0 @@
-  PPPPP   IIIIIII   N    N
-  P   PP     I      NN   N   IDENTIFICATION
-  P   PP     I      N N  N
-  PPPPP      I      N  N N      PROGRAM
-  P          I      N   NN
-  P       IIIIIII   N    N
-
-  Strike a key when ready ...
