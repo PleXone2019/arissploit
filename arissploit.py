c1bc33372ad00b83d3fd27f77ff57e8e22f06ca0
Entynetproject <43011806+entynetproject@users.noreply.github.com>
2019-07-11 14:18:07 +0200

    Update arissploit.py

--- arissploit.py
+++ arissploit.py
@@ -50,13 +50,6 @@ O = '\033[0;33m' # Orange
 B = '\033[1;34m' #Blue
 E = '\033[0m' # End
 
-name = os.popen('whoami').read() 
-
-if name == "root":
-	time.sleep(0)
-else:
-	exit()
-
 print "\033]2;Arissploit Framework\a"
 def show_graphic():
     os.system("cat /etc/arissploit/banner/banner.txt")
