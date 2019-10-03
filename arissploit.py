dda1c86c709734007e017775c4bf47f2e8eb3348
Ivan Nikolsky <54115104+enty8080@users.noreply.github.com>
2019-10-03 21:51:47 +0300

    Update arissploit.py

--- arissploit.py
+++ arissploit.py
@@ -50,7 +50,7 @@ O = '\033[0;33m' # Orange
 B = '\033[1;34m' #Blue
 E = '\033[0m' # End
 
-print "\033]2;Arissploit Framework\a"
+os.system("printf '\033]2;Arissploit Framework\a'")
 def show_graphic():
     os.system("cat banner/banner.txt")
 show_graphic()
