f0a6e1e74bfb8b8f8d63c538b544b9e66b4ee78c
Ivan Nikolsky <54115104+enty8080@users.noreply.github.com>
2019-10-06 20:28:13 +0300

    Update header.py

--- core/header.py
+++ core/header.py
@@ -4,17 +4,17 @@ from core.moduleop import count
 
 arissploit = r"""
 
-                 .----.   @   @                  
-                / .-.-.`.  \v/                            
-                | | '\ \ \_/ )                      
-               ,-\ `-.' /.'  /           
-               '---`----'----'    
+                .----.   @   @                  
+               / .-.-.`.  \v/                            
+               | | '\ \ \_/ )                      
+              ,-\ `-.' /.'  /           
+              '---`----'----'    
 """
 
 def print_info():
 	count()
 
-	print("\t" + colors.bold + "     Arissploit Framework\n" + colors.end)
+	print("\t" + colors.bold + "    Arissploit Framework\n" + colors.end)
 	print("\t" + colors.bold + "Core "+colors.end+"\t[ "+info.version+" "+info.codename+" ]" + colors.end)
 	print("\t" + colors.bold + "API"+colors.end+"\t[ "+info.apiversion+"       ]"+colors.end)
 	print("\t" + colors.bold + "Date"+colors.end+"\t[ "+info.update_date+"          ]"+colors.end)
