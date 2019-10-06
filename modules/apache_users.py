90779bdc1177ec866118538d39531b014d9a27ba
Ivan Nikolsky <54115104+enty8080@users.noreply.github.com>
2019-10-06 17:58:10 +0300

    Update apache_users.py

--- modules/apache_users.py
+++ modules/apache_users.py
@@ -18,7 +18,7 @@ conf = {
 
 # List of the variables
 variables = OrderedDict((
-	("target", ["google.com", "Target address."]),
+	("target", ["google.com", "target address."]),
 ))
 
 # Simple changelog
