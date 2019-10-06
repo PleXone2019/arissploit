00ee4118e8e53c79dd8b60c133ce5540b1a3bb0a
Ivan Nikolsky <54115104+enty8080@users.noreply.github.com>
2019-10-06 18:37:07 +0300

    Update hostname_resolver.py

--- modules/hostname_resolver.py
+++ modules/hostname_resolver.py
@@ -1,15 +1,11 @@
-
-
 from core.arissploit import *
 import socket
 
 conf = {
 	"name": "hostname_resolver",
 	"version": "1.0",
-	"shortdesc": "resolve hostname using ip",
-	"author": "entynetproject",
-	"github": "entynetproject",
-	"email": "entynetproject0@gmail.com",
+	"shortdesc": "Resolve hostname using ip.",
+	"author": "Entynetproject",
 	"initdate": "9.5.2016",
 	"lastmod": "3.1.2017",
 	"apisupport": True
@@ -17,7 +13,7 @@ conf = {
 
 # List of the variables
 variables = OrderedDict((
-	('target', ['192.168.1.1', 'target ip address']),
+	('target', ['192.168.1.1', 'Target ip address.']),
 ))
 
 #simple changelog
@@ -26,11 +22,11 @@ changelog = "Version 1.0:\nrelease"
 def run():
 	try:
 		querly = socket.gethostbyaddr(variables['target'][0])
-		printSuccess("resolved hostname: "+ querly[0])
+		printSuccess("Resolved hostname: "+ querly[0])
 		return querly[0]
 	except(socket.herror):
-		printError("unknown host")
-		return ModuleError("unknown host")
+		printError("Unknown host!")
+		return ModuleError("Unknown host!")
 	except(socket.gaierror):
-		printError("name or service not known")
-		return ModuleError("name or service not known")
+		printError("Name or service not known!")
+		return ModuleError("Name or service not known!")
