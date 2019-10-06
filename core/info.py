c660b18b1ee1026dabd4d85c6397b1274071bea0
Ivan Nikolsky <54115104+enty8080@users.noreply.github.com>
2019-10-06 19:54:13 +0300

    Update info.py

--- core/info.py
+++ core/info.py
@@ -5,7 +5,4 @@ apiversion = "1.0.1-alpha"
 update_date = "3.1.2017"
 codename = "uranium"
 
-about = ("Arissploit Framework "+version+" "+codename+
-"\nauthor: Noa-Emil Nissinen (entynetproject)"
-"\nemail: entynetproject0@gmail.com"
-"\ngithub: entynetproject")
+about = ("")
