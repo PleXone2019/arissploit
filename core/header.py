662bfee61c691d7d746d5e24dd456891ec5891f7
Ivan Nikolsky <54115104+enty8080@users.noreply.github.com>
2019-10-07 21:12:26 +0300

    Update header.py

--- core/header.py
+++ core/header.py
@@ -15,8 +15,8 @@ def print_info():
 	count()
 
 	print("\t" + colors.bold + "    Arissploit Framework\n" + colors.end)
-	print("\t" + colors.bold + "Core "+colors.end+"\t[ "+info.version+" "+info.codename+" ]" + colors.end)
-	print("\t" + colors.bold + "API"+colors.end+"\t[ "+info.apiversion+"       ]"+colors.end)
-	print("\t" + colors.bold + "Date"+colors.end+"\t[ "+info.update_date+"          ]"+colors.end)
-	print("\t" + colors.bold + "Modules "+colors.end+"[ "+count.mod+" modules"+"        ]"+colors.end)
+	print("\t" + colors.bold + "Core "+colors.end+"\t[ "+info.version+" "+info.codename+"      ]" + colors.end)
+	print("\t" + colors.bold + "API"+colors.end+"\t[ "+info.apiversion+"            ]"+colors.end)
+	print("\t" + colors.bold + "Date"+colors.end+"\t[ "+info.update_date+"               ]"+colors.end)
+	print("\t" + colors.bold + "Modules "+colors.end+"[ "+count.mod+" modules"+"             ]"+colors.end)
 	print("\n")
