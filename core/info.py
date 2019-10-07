94cb1de8ac0560d450fb0841b00f2e9b6025476d
Ivan Nikolsky <54115104+enty8080@users.noreply.github.com>
2019-10-07 21:11:47 +0300

    Update info.py

--- core/info.py
+++ core/info.py
@@ -1,8 +1,8 @@
 from core import colors
 
-version = "3.1-alpha"
-apiversion = "1.0.1-alpha"
-update_date = "3.1.2017"
-codename = "uranium"
+version = "3.1"
+apiversion = "1.0.1"
+update_date = "3.1.2019"
+codename = ""
 
 about = ("")
