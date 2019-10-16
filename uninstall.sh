9d96fdddd4ad62e78189304197864fad46535d30
Ivan Nikolsky <54115104+enty8080@users.noreply.github.com>
2019-10-16 23:06:20 +0300

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -37,3 +37,41 @@ rm /bin/arissploit
 rm /usr/local/bin/arissploit
 rm -r ~/arissploit
 } &> /dev/null
+
+echo -e
+echo -e
+
+function ProgressBar {
+
+    let _progress=(${1}*100/${2}*100)/100
+    let _done=(${_progress}*4)/10
+    let _left=40-$_done
+
+    _fill=$(printf "%${_done}s")
+    _empty=$(printf "%${_left}s")
+
+                         
+        
+printf "\rUninstalling: [${_fill// /\#}${_empty// /-}] ${_progress}%%"
+
+}
+
+
+_start=1
+
+_end=100
+
+for number in $(seq ${_start} ${_end})
+do
+    sleep 0.2
+    ProgressBar ${number} ${_end}
+done
+
+echo -e
+echo -e
+		echo -e "["$YS"inf"$CE"] "$YS"Done"$CE""
+		sleep 2
+		echo -e "Press "$YS"any key"$CE" to exit..."
+		read
+		clear
+		exit
